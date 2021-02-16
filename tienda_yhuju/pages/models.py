c643c030370d925ecb996cf94913d0907820ead1
manuel <jgmanuelrivera00@gmail.com>
2021-02-16 11:56:40 -0500

    modificacion de models

--- tienda_yhuju/pages/models.py
+++ tienda_yhuju/pages/models.py
@@ -4,7 +4,10 @@ from ckeditor.fields import RichTextField
 class Page(models.Model):
     title = models.CharField(verbose_name="Título", max_length=200)
     content = RichTextField(verbose_name="Contenido")
-    order = models.SmallIntegerField(verbose_name="Orden", default=0)
+    order = models.SmallIntegerField(verbose_name="order", default=0)
+    decimal = models.DecimalField(verbose_name="Kilogramo",max_digits=6, decimal_places=2)
+    pcompra = models.DecimalField(verbose_name="Precio Compra",max_digits=6, decimal_places=2)
+    pventa = models.DecimalField(verbose_name="Precio Venta",max_digits=6, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
