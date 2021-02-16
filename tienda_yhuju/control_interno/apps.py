9d801ad3c75301e98ab03a4177e2772fd840e247
manuel <jgmanuelrivera00@gmail.com>
2021-02-16 10:27:14 -0500

    se agrego pages para crud

--- tienda_yhuju/control_interno/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ControlInternoConfig(AppConfig):
-    name = 'control_interno'
--- tienda_yhuju/control_interno/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-def control_interno(request):
-    return render(request, "core/index.html")
-
-def proceso_interno(request):
-    return render(request, "core/procesos.html")
-
-
