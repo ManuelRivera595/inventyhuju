a548a81df1a8b8ba1eeff42ed576baf53af2eb9f
manuel <jgmanuelrivera00@gmail.com>
2021-02-16 22:39:38 -0500

    se agrego actualizacion y delete

--- tienda_yhuju/pages/views.py
+++ tienda_yhuju/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page
 
@@ -15,4 +15,15 @@ class PageDetailView(DetailView):
 class PageCreate(CreateView):
     model = Page
     fields = ['title', 'content', 'order', 'decimal', 'pcompra', 'pventa']
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order', 'decimal', 'pcompra', 'pventa']
+    template_name_suffix = '_update_form'
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+class PageDelete(DeleteView):
+    model = Page
     success_url = reverse_lazy('pages:pages')
